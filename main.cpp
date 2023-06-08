4c2d8b4a7d1e769f03d89317a81e83d9a4e9206b
Danila Slesarev <blackdeer1524@yandex.ru>
2023-06-09 01:38:26 +0300

    renamed few variables

--- main.cpp
+++ main.cpp
@@ -6,143 +6,154 @@
 #include <ostream>
 #include <vector>
 
-std::vector<std::vector<int>> nodes;
-std::vector<std::vector<int>> variantsOfGroup;
-std::vector<int>              g1;
-std::vector<int>              g2;
-int                           n;
+enum class CrewmemberStatus { FREE, FIRST_GROUP, SECOND_GROUP };
 
-auto                          makeVariants(std::vector<int> groupNums,
-                                           int              universal,
-                                           int              first_group_n,
-                                           int              second_group_n,
-                                           int              current_crewmember_i) -> void {
+std::vector<std::vector<bool>>             nodes;
+std::vector<std::vector<CrewmemberStatus>> group_variants;
+std::vector<uint64_t>                      people_needed_for_first_group;
+std::vector<uint64_t>                      first_group_sizes;
+uint64_t                                   n;
+
+auto make_variants(std::vector<CrewmemberStatus> group,
+                   uint64_t                      universals_n,
+                   uint64_t                      first_group_n,
+                   uint64_t                      second_group_n,
+                   uint64_t                      current_crewmember_i) -> void {
     if (current_crewmember_i == n) {
-        variantsOfGroup.push_back(groupNums);
-        g1.push_back(universal);
-        g2.push_back(first_group_n);
+        group_variants.push_back(group);
+        first_group_sizes.push_back(first_group_n);
         return;
     }
-    int i = 0;
+    uint64_t i = 0;
     for (; i < n; ++i) {
         if (nodes[current_crewmember_i][i] == 1) {
             break;
         }
     }
     if (i == n) {
-        groupNums[current_crewmember_i] = 0;
-        makeVariants(groupNums,
-                     universal + 1,
-                     first_group_n,
-                     second_group_n,
-                     current_crewmember_i + 1);
+        group[current_crewmember_i] = CrewmemberStatus::FREE;
+        make_variants(group,
+                      universals_n + 1,
+                      first_group_n,
+                      second_group_n,
+                      current_crewmember_i + 1);
         return;
     }
-    auto group1 = std::vector<int>(groupNums.size());
-    auto group2 = std::vector<int>(groupNums.size());
-    for (uint64_t k = 0; k < groupNums.size(); ++k) {
-        const auto x = groupNums[k];
+    auto group1 = std::vector<CrewmemberStatus>(group.size());
+    auto group2 = std::vector<CrewmemberStatus>(group.size());
+    for (uint64_t k = 0; k < group.size(); ++k) {
+        const auto x = group[k];
         group1.at(k) = x;
         group2.at(k) = x;
     }
-    group1[current_crewmember_i]    = 1;
-    group2[current_crewmember_i]    = 2;
+    group1[current_crewmember_i]    = CrewmemberStatus::FIRST_GROUP;
+    group2[current_crewmember_i]    = CrewmemberStatus::SECOND_GROUP;
     auto can_add_crewmate_to_first  = true;
     auto can_add_crewmate_to_second = true;
     for (; i < n && (can_add_crewmate_to_first || can_add_crewmate_to_second);
          ++i) {
         if (nodes[current_crewmember_i][i] == 1) {
-            if (groupNums[i] == 0) {
-                group1[i] = 2;
-                group2[i] = 1;
-            } else if (groupNums[i] == 1) {
+            if (group[i] == CrewmemberStatus::FREE) {
+                group1[i] = CrewmemberStatus::SECOND_GROUP;
+                group2[i] = CrewmemberStatus::FIRST_GROUP;
+            } else if (group[i] == CrewmemberStatus::FIRST_GROUP) {
                 can_add_crewmate_to_first = false;
-            } else if (groupNums[i] == 2) {
+            } else if (group[i] == CrewmemberStatus::SECOND_GROUP) {
                 can_add_crewmate_to_second = false;
             }
         }
     }
     if (can_add_crewmate_to_first) {
-        makeVariants(group1,
-                     universal,
-                     first_group_n + 1,
-                     second_group_n,
-                     current_crewmember_i + 1);
+        make_variants(group1,
+                      universals_n,
+                      first_group_n + 1,
+                      second_group_n,
+                      current_crewmember_i + 1);
     }
     if (can_add_crewmate_to_second) {
-        makeVariants(group2,
-                     universal,
-                     first_group_n,
-                     second_group_n + 1,
-                     current_crewmember_i + 1);
+        make_variants(group2,
+                      universals_n,
+                      first_group_n,
+                      second_group_n + 1,
+                      current_crewmember_i + 1);
     }
 }
 
 auto main() -> int {
     std::cin >> n;
     nodes.resize(n);
-    std::vector<int> base(n);
-    std::string      str;
+    std::vector<CrewmemberStatus> base(n, CrewmemberStatus::FREE);
+    std::string                   str;
 
     getline(std::cin, str);
-    for (int i = 0; i < n; ++i) {
+    for (uint64_t i = 0; i < n; ++i) {
         nodes[i].reserve(n);
         getline(std::cin, str);
         for (const auto chr : str) {
             switch (chr) {
                 case '+':
-                    nodes[i].push_back(1);
+                    nodes[i].push_back(true);
                     break;
                 case '-':
-                    nodes[i].push_back(0);
+                    nodes[i].push_back(false);
                     break;
             }
         }
     }
 
-    makeVariants(base, 0, 0, 0, 0);
-    if (variantsOfGroup.empty()) {
+    make_variants(base, 0, 0, 0, 0);
+    if (group_variants.empty()) {
         std::cout << "No solution" << std::endl;
         return EXIT_SUCCESS;
     }
 
-    for (uint64_t i = 0; i < variantsOfGroup.size(); ++i) {
-        g1[i] = n / 2 - g2[i];
+    people_needed_for_first_group.resize(first_group_sizes.size(), 0);
+    const auto average_group_size = n >> 1;
+    for (uint64_t i = 0; i < group_variants.size(); ++i) {
+        if (average_group_size > first_group_sizes[i]) {
+            people_needed_for_first_group[i] =
+                average_group_size - first_group_sizes[i];
+        } else {
+            people_needed_for_first_group[i] = 0;
+        }
     }
 
-    std::vector<std::vector<int>> variants(variantsOfGroup.size());
-    for (int i = 0; i < n; ++i) {
-        for (uint64_t j = 0; j < variantsOfGroup.size(); ++j) {
-            auto x = variantsOfGroup.at(j);
-            if (x[i] == 0 && g1[j] > 0) {
-                x[i] = 1;
-                g1[j]--;
+    std::vector<std::vector<CrewmemberStatus>> variants(group_variants.size());
+    for (uint64_t group_i = 0; group_i < group_variants.size(); ++group_i) {
+        for (uint64_t crewmate_i = 0; crewmate_i < n; ++crewmate_i) {
+            auto current_group = group_variants.at(group_i);
+            if (current_group[crewmate_i] == CrewmemberStatus::FREE &&
+                people_needed_for_first_group[group_i] > 0) {
+                current_group[crewmate_i] = CrewmemberStatus::FIRST_GROUP;
+                --people_needed_for_first_group[group_i];
             }
-            if (x[i] == 0 && g1[j] == 0) {
-                x[i] = 2;
+            if (current_group[crewmate_i] == CrewmemberStatus::FREE &&
+                people_needed_for_first_group[group_i] == 0) {
+                current_group[crewmate_i] = CrewmemberStatus::SECOND_GROUP;
             }
-            variants[j].push_back(x[i]);
+            variants[group_i].push_back(current_group[crewmate_i]);
         }
     }
 
     auto ans = variants[0];
-    for (const auto &x : variantsOfGroup) {
-        if (x.size() <= ans.size()) {
-            for (uint64_t i = 0; i < x.size(); ++i) {
-                if (ans[i] > x[i]) {
-                    ans = x;
-                    break;
-                }
-                if (ans[i] < x[i]) {
-                    break;
-                }
+    for (const auto &x : group_variants) {
+        if (x.size() > ans.size()) {
+            continue;
+        }
+        for (uint64_t i = 0; i < x.size(); ++i) {
+            if (ans[i] > x[i]) {
+                ans = x;
+                break;
+            }
+            if (ans[i] < x[i]) {
+                break;
             }
         }
     }
 
     for (uint64_t i = 0; i < ans.size(); ++i) {
         const auto x = ans[i];
-        if (x == 1) {
+        if (x == CrewmemberStatus::FIRST_GROUP) {
             std::cout << i + 1 << ' ';
         }
     }
