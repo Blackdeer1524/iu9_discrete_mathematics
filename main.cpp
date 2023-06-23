ff5a1c292464c9a0bb603f7c5e31253bae375414
Danila Slesarev <blackdeer1524@yandex.ru>
2023-06-24 01:48:14 +0300

    second try

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 #include <vector>
 
 using transitions_table = std::vector<std::vector<uint64_t>>;
-using outputs_table     = std::vector<std::vector<std::string>>;
+using outputs_table     = std::vector<std::vector<uint64_t>>;
 
 struct Automaton {
     transitions_table delta;
@@ -48,7 +48,7 @@ auto main() -> int {
 
     transitions_table delta(states_count,
                             std::vector<uint64_t>(in_alphabet_size));
-    outputs_table phi(states_count, std::vector<std::string>(in_alphabet_size));
+    outputs_table phi(states_count, std::vector<uint64_t>(in_alphabet_size));
 
     for (uint64_t i = 0; i < states_count; ++i) {
         for (uint64_t j = 0; j < in_alphabet_size; ++j) {
@@ -60,7 +60,7 @@ auto main() -> int {
 
     for (uint64_t i = 0; i < states_count; ++i) {
         for (uint64_t j = 0; j < in_alphabet_size; ++j) {
-            std::string output;
+            uint64_t output;
             in >> output;
             phi.at(i).at(j) = output;
         }
@@ -68,7 +68,7 @@ auto main() -> int {
 
     Automaton a{delta, phi, states_count, in_alphabet_size};
 
-    std::vector<std::unordered_set<std::string>> z(states_count);
+    std::vector<std::unordered_set<uint64_t>> z(states_count);
     for (uint64_t i = 0; i < a.states_count; ++i) {
         for (uint64_t j = 0; j < a.alphabet_size; ++j) {
             const auto next_state = a.delta.at(i).at(j);
@@ -83,8 +83,9 @@ auto main() -> int {
     std::unordered_map<std::string, uint64_t> moore_state_name2index;
     uint64_t                                  moore_states_count = 0;
     for (uint64_t q = 0; q < z.size(); ++q) {
-        for (const auto &y : z.at(q)) {
-            const auto name              = create_name(q, y);
+        for (const auto y_i : z.at(q)) {
+            const auto &y                = out_alphabet.at(y_i);
+            const auto  name             = create_name(q, y);
             moore_state_name2index[name] = moore_states_count;
             std::cout << "    " << moore_states_count
                       << "[label = \"" + name + "\"]" << std::endl;
@@ -96,16 +97,18 @@ auto main() -> int {
         const auto z_class = z.at(z_class_i);
         for (uint64_t in_signal_i = 0; in_signal_i < a.alphabet_size;
              ++in_signal_i) {
-            const auto in_signal        = in_alphabet.at(in_signal_i);
+            const auto  in_signal       = in_alphabet.at(in_signal_i);
 
-            const auto next_state       = a.delta.at(z_class_i).at(in_signal_i);
-            const auto output           = a.phi.at(z_class_i).at(in_signal_i);
-            const auto next_moore_state = create_name(next_state, output);
-            const auto next_moore_state_i =
+            const auto  next_state      = a.delta.at(z_class_i).at(in_signal_i);
+            const auto  output_signal_i = a.phi.at(z_class_i).at(in_signal_i);
+            const auto &output          = out_alphabet.at(output_signal_i);
+            const auto  next_moore_state = create_name(next_state, output);
+            const auto  next_moore_state_i =
                 moore_state_name2index.at(next_moore_state);
-            for (const auto &y : z_class) {
-                const auto cur_moore_state = create_name(z_class_i, y);
-                const auto cur_moore_state_i =
+            for (const auto y_i : z_class) {
+                const auto &y               = out_alphabet.at(y_i);
+                const auto  cur_moore_state = create_name(z_class_i, y);
+                const auto  cur_moore_state_i =
                     moore_state_name2index.at(cur_moore_state);
                 std::cout << "    " << cur_moore_state_i << " -> "
                           << next_moore_state_i
